7b472b672684e254512bfec25e529e0d10644e64
ka <todo1@rotarydef.net>
2023-09-15 10:03:50 -0700

    force option

--- CMakeFetchZip.cmake
+++ CMakeFetchZip.cmake
@@ -3,13 +3,16 @@ macro(CMakeFetchZip)
     
     set(${PROJECT_NAME}_m_evacu ${m})
     set(m CMakeFetchZip)
-    cmake_parse_arguments(${m} "CLEAN" "DESTINATION;SOURCE;WORKING_DIRECTORY" "" ${ARGV})
-    list(APPEND ${m}_unsetter ${m}_DESTINATION ${m}_SOURCE ${m}_WORKING_DIRECTORY ${m}_CLEAN_WARKING_DIRECTORY )
+    cmake_parse_arguments(${m} "FORCE_UPDATE;FORCE_EXTRACT" "DESTINATION;SOURCE;WORKING_DIRECTORY" "" ${ARGV})
+    list(APPEND ${m}_unsetter ${m}_DESTINATION ${m}_SOURCE ${m}_WORKING_DIRECTORY ${m}_CLEAN_WARKING_DIRECTORY ${m}_FORCE_UPDATE ${m}_FORCE_EXTRACT)
     
     list(APPEND ${m}_unsetter ${m}_want_src ${m}_dest ${m}_download_dir ${m}_clean)
     set(${m}_want_src ${${m}_SOURCE})
     set(${m}_dest ${${m}_DESTINATION})
     set(${m}_clean ${${m}_CLEAN})
+    set(${m}_force ${${m}_FORCE_UPDATE})
+    set(${m}_force_extract ${${m}_FORCE_EXTRACT})
+    
     
     if(DEFINED ${m}_WORKING_DIRECTORY AND (EXISTS ${${m}_WORKING_DIRECTORY}))
         set(${m}_download_dir "${${m}_WORKING_DIRECTORY}")
@@ -21,23 +24,28 @@ macro(CMakeFetchZip)
     get_filename_component(${m}_name ${${m}_want_src} NAME)
     set(${m}_want "${${m}_download_dir}/${${m}_name}")
     file(MAKE_DIRECTORY "${${m}_dest}")
-    if(NOT DEFINED KAUTIL_JDK_ROOT OR NOT EXISTS "${KAUTIL_JDK_ROOT}")
-        if(${MINGW})
-            if( (NOT DEFINED CACHE{${${m}_want}}))
-                file(DOWNLOAD "${${m}_want_src}" "${${m}_want}")
-                set(${${m}_want} TRUE CACHE STRING "")
-            endif()
-            
-            if((NOT DEFINED CACHE{${${m}_dest}.${${m}_name}}) OR (NOT EXISTS "${${m}_dest}"))
-                execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf "${${m}_want}" WORKING_DIRECTORY ${${m}_dest})
-                set(${${m}_dest}.${${m}_name} TRUE CACHE STRING "")
-            endif()
-        endif()
+    
+    if(${${m}_force})
+        unset(${${m}_want} CACHE)
+        unset(${${m}_dest}.${${m}_name} CACHE)
     endif()
     
-    if(${${m}_clean})
-        message(${${m}_want})
-        file(REMOVE_RECURSE "${${m}_want}")
+    if(${${m}_force_extract})
+        unset(${${m}_dest}.${${m}_name} CACHE)
+    endif()
+    
+        
+    if(NOT DEFINED KAUTIL_JDK_ROOT OR NOT EXISTS "${KAUTIL_JDK_ROOT}")
+        
+        if( (NOT DEFINED CACHE{${${m}_want}}) OR NOT EXISTS "${${m}_want}")
+            file(DOWNLOAD "${${m}_want_src}" "${${m}_want}")
+            set(${${m}_want} TRUE CACHE STRING "")
+        endif()
+        
+        if(NOT DEFINED CACHE{${${m}_dest}.${${m}_name}})
+            execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf "${${m}_want}" WORKING_DIRECTORY ${${m}_dest})
+            set(${${m}_dest}.${${m}_name} TRUE CACHE STRING "")
+        endif()
     endif()
     
     foreach(__v ${${m}_unsetter})
--- readme.md
+++ readme.md
@@ -1,6 +1,13 @@
 ### CMakeFetchZip
 * download zip and extract it.
 
+### note
+* SOURCE : uri to zip to download
+* DESTINATION : destination dir to where contents expand
+* WORKING_DIRECTORY : download directory for zip
+* FORCE_EXTRACT : force to extract 
+* FORCE_UPDATE : force to download and extract 
+
 ### example
 ```cmake
 include(CMakeFetchZip.cmake)
@@ -8,6 +15,7 @@ CMakeFetchZip(
     SOURCE path_to_zip
     DESTINATION path_to_extract_dir
     WORKING_DIRECTORY path_to_donwload_dir
-    CLEAN
+    #FORCE_EXTRACT
+    #FORCE_UPDATE 
 )
 ```
